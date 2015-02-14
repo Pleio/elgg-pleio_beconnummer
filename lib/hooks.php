e81e916c77cbe39b09298037706f1d8a2f0aeb66
Bart Jeukendrup <bart@jeukendrup.nl>
2015-02-14 12:57:26 +0100

    security fix

--- lib/hooks.php
+++ lib/hooks.php
@@ -1,63 +1,54 @@
 <?php 
+/**
+ * Validate any beconnumber profile field if the content is valid
+ * 
+ * @param string $hook
+ * @param string $type
+ * @param mixed $return_value
+ * @param mixed $params
+ */
 
-	/**
-	 * Validate any beconnumber profile field if the content is valid
-	 * 
-	 * @param string $hook
-	 * @param string $type
-	 * @param mixed $return_value
-	 * @param mixed $params
-	 */
-	function pleio_beconnummer_action_handler($hook, $type, $return_value, $params){
-		
-		if($profile_fields = elgg_get_config("profile_fields")){
-			$check_fields = array();
+function pleio_beconnummer_action_handler($hook, $type, $return_value, $params){
+	$options = array(
+			"type" => "object",
+			"subtype" => CUSTOM_PROFILE_FIELDS_PROFILE_SUBTYPE,
+			"limit" => false,
+			"owner_guid" => elgg_get_config("site_guid")
+	);
+	$profile_fields = elgg_get_entities($options);
+
+	$check_fields = array();
+	
+	foreach($profile_fields as $profile_field){
+		if($profile_field->metadata_type == "beconnummer"){
+			$check_fields[] = $profile_field->metadata_name;
+		}
+	}
+
+	if (empty($check_fields)) {
+		return true;
+	}
+	
+	foreach($check_fields as $metadata_name){
+		if($type == "profile/edit") {
+			if($value = get_input($metadata_name)){
+				if(!pleio_beconnummer_validate_beconnummer($value)){
+					// let the user know this number was invalid
+					register_error(elgg_echo("pleio_beconnummer:profile_field:error:invalid_number"));
 			
-			foreach($profile_fields as $metadata_name => $field_type){
-				if($field_type == "beconnummer"){
-					$check_fields[] = $metadata_name;
+					$user = get_user(get_input("guid"));
+					// set value to previous value
+					set_input($metadata_name, $user->$metadata_name);
 				}
 			}
-			
-			if(!empty($check_fields)){
-				foreach($check_fields as $metadata_name){
-					if($type == "profile/edit") {
-						if($value = get_input($metadata_name)){
-							if(!pleio_beconnummer_validate_beconnummer($value)){
-								// let the user know this number was invalid
-								register_error(elgg_echo("pleio_beconnummer:profile_field:error:invalid_number"));
-						
-								$user = get_user(get_input("guid"));
-								// set value to previous value
-								set_input($metadata_name, $user->$metadata_name);
-							}
-						}
-					} elseif($type == "register") {
-						if($value = get_input("custom_profile_fields_" . $metadata_name)){
-							if(!pleio_beconnummer_validate_beconnummer($value)){
-								// let the user know this number was invalid
-								register_error(elgg_echo("pleio_beconnummer:profile_field:error:invalid_number"));
-								
-								// set value to empty
-								// because of profile_manager we have to change _POST
-								unset($_POST["custom_profile_fields_" . $metadata_name]);
-								unset($_REQUEST["custom_profile_fields_" . $metadata_name]);
-							}
-						}
-					} elseif (in_array($type, array("subsites/join/request_approval", "subsites/join/validate_domain", "subsites/join/missing_fields"))) {
-						if($value = get_input("custom_profile_fields_" . $metadata_name)){
-							if(!pleio_beconnummer_validate_beconnummer($value)){
-								// let the user know this number was invalid
-								register_error(elgg_echo("pleio_beconnummer:profile_field:error:invalid_number"));
-								
-								// set value to empty
-								// because of profile_manager we have to change _POST
-								$_POST["custom_profile_fields_" . $metadata_name] = "";
-								$_REQUEST["custom_profile_fields_" . $metadata_name] = "";
-							}
-						}
-					}
+		} elseif (in_array($type, array("register", "subsites/join/request_approval", "subsites/join/validate_domain", "subsites/join/missing_fields"))) {
+			if($value = get_input("custom_profile_fields_" . $metadata_name)){
+				if(!pleio_beconnummer_validate_beconnummer($value)){
+					// let the user know this number was invalid
+					register_error(elgg_echo("pleio_beconnummer:profile_field:error:invalid_number"));
+					return false;
 				}
 			}
 		}
 	}
+}
