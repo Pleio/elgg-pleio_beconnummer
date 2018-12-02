12d647b80164373f68b2123b391b97d616408184
Bart Jeukendrup <bart@jeukendrup.nl>
2018-12-02 15:44:50 +0100

    Get already set number when field is not present

--- lib/hooks.php
+++ lib/hooks.php
@@ -19,19 +19,24 @@ function pleio_beconnummer_action_handler($hook, $type, $return_value, $params){
 	set_input('accesslevel', $accesslevel);
 
 	if ($type == "profile/edit") {
+		$user = get_user(get_input("guid"));
 		$value = get_input($metadata_name);
 	} elseif ($type == "profile_manager/complete") {
+		$user = elgg_get_logged_in_user_entity();
 		$fields = get_input('custom_profile_fields');
 		$value = $fields[$metadata_name];
 	} else {
 		$value = get_input("custom_profile_fields_" . $metadata_name);
 	}
 
+	if (!$value && $user) {
+		$value = $user->$metadata_name;
+	}
+
 	if (!pleio_beconnummer_validate_beconnummer($value)) {
 		register_error(elgg_echo("pleio_beconnummer:profile_field:error:invalid_number"));
 
 		if ($type == "profile/edit") {
-			$user = get_user(get_input("guid"));
 			set_input($metadata_name, $user->$metadata_name);
 		} else {
 			forward(REFERER);
